0257ecd1b472182dbbac4ef5f6a25779e6c9237c
stephenkao <stephen.yuchen.kao@gmail.com>
2019-08-14 15:59:33 -0400

    feat: only load image if not preloaded already

--- croppie.js
+++ croppie.js
@@ -208,6 +208,16 @@
     });
   }
 
+  function loadImageIfNeeded(src, doExif, img) {
+    return new Promise(function (resolve) {
+      if (img) {
+        resolve(img);
+      } else {
+        resolve(loadImage(src, doExif));
+      }
+    });
+  }
+
   function naturalImageDimensions(img, ornt) {
     var w = img.naturalWidth;
     var h = img.naturalHeight;
@@ -1133,50 +1143,51 @@
     // HACK
     self.data.orientation = options.orientation;
 
-    return loadImage(url, hasExif).then(function (img) {
-      _replaceImage.call(self, img);
-      if (!points.length) {
-        var natDim = naturalImageDimensions(img);
-        var rect = self.elements.viewport.getBoundingClientRect();
-        var aspectRatio = rect.width / rect.height;
-        var imgAspectRatio = natDim.width / natDim.height;
-        var width, height;
+    var img = options.img;
 
-        if (imgAspectRatio > aspectRatio) {
-          height = natDim.height;
-          width = height * aspectRatio;
-        }
-        else {
-          width = natDim.width;
-          height = natDim.height / aspectRatio;
-        }
+    _replaceImage.call(self, img);
 
-        var x0 = (natDim.width - width) / 2;
-        var y0 = (natDim.height - height) / 2;
-        var x1 = x0 + width;
-        var y1 = y0 + height;
-        self.data.points = [x0, y0, x1, y1];
+    if (!points.length) {
+      var natDim = naturalImageDimensions(img);
+      var rect = self.elements.viewport.getBoundingClientRect();
+      var aspectRatio = rect.width / rect.height;
+      var imgAspectRatio = natDim.width / natDim.height;
+      var width, height;
+
+      if (imgAspectRatio > aspectRatio) {
+        height = natDim.height;
+        width = height * aspectRatio;
       }
-      else if (self.options.relative) {
-        points = [
-          points[0] * img.naturalWidth / 100,
-          points[1] * img.naturalHeight / 100,
-          points[2] * img.naturalWidth / 100,
-          points[3] * img.naturalHeight / 100
-        ];
+      else {
+        width = natDim.width;
+        height = natDim.height / aspectRatio;
       }
 
-      self.data.orientation = options.orientation || 1;
-      self.data.points = points.map(function (p) {
-        return parseFloat(p);
-      });
-      if (self.options.useCanvas) {
-        _transferImageToCanvas.call(self, self.data.orientation);
-      }
-      _updatePropertiesFromImage.call(self);
-      _triggerUpdate.call(self);
-      cb && cb();
+      var x0 = (natDim.width - width) / 2;
+      var y0 = (natDim.height - height) / 2;
+      var x1 = x0 + width;
+      var y1 = y0 + height;
+      self.data.points = [x0, y0, x1, y1];
+    }
+    else if (self.options.relative) {
+      points = [
+        points[0] * img.naturalWidth / 100,
+        points[1] * img.naturalHeight / 100,
+        points[2] * img.naturalWidth / 100,
+        points[3] * img.naturalHeight / 100
+      ];
+    }
+
+    self.data.orientation = options.orientation || 1;
+    self.data.points = points.map(function (p) {
+      return parseFloat(p);
     });
+    if (self.options.useCanvas) {
+      _transferImageToCanvas.call(self, self.data.orientation);
+    }
+    _updatePropertiesFromImage.call(self);
+    _triggerUpdate.call(self);
+    cb && cb();
   }
 
   function fix(v, decimalPoints) {
